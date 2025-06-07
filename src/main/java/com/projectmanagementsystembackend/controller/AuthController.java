1c33bdbe213532ac99f806ed93c9c24925e08992
YASHPATEL\patel <patelyash2462@gmail.com>
2025-06-07 19:30:34 +0530

    support for login with otp

--- src/main/java/com/projectmanagementsystembackend/controller/AuthController.java
+++ src/main/java/com/projectmanagementsystembackend/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.projectmanagementsystembackend.model.User;
 import com.projectmanagementsystembackend.repository.UserRepository;
 import com.projectmanagementsystembackend.request.LoginRequest;
 import com.projectmanagementsystembackend.service.CustomUserDetailsImpl;
+import com.projectmanagementsystembackend.service.EmailService;
+import com.projectmanagementsystembackend.service.OtpService;
 import com.projectmanagementsystembackend.service.SubscriptionService;
 import com.projectmanagementsystembackend.vo.AuthResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +18,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/auth")
@@ -32,6 +33,10 @@ public class AuthController {
     private SubscriptionService subscriptionService;
     @Autowired
     private CustomUserDetailsImpl customUserDetails;
+    @Autowired
+    private OtpService otpService;
+    @Autowired
+    private EmailService emailService;
 
     // signup api
     @PostMapping("/signup")
@@ -63,36 +68,84 @@ public class AuthController {
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody LoginRequest loginRequest){
-
-        String username = loginRequest.getEmail();
-        String password = loginRequest.getPassword();
-        Authentication authentication = authenticate(username,password);
-
-        Authentication authentication1 = new UsernamePasswordAuthenticationToken(username,password);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String jwt = JwtProvider.generateToken(authentication);
+    @PostMapping("/send-otp/{email}")
+    public ResponseEntity<Object> sendOtp(@PathVariable(value = "email") String email) {
         AuthResponse authResponse = new AuthResponse();
-        authResponse.setJwt(jwt);
-        authResponse.setMessage("User Logged in successfully");
-        authResponse.setStatus(200);
-
-        return new ResponseEntity<>(authResponse,HttpStatus.OK);
+        try {
+            String otp = otpService.generateOtp(email);
+            emailService.sendOtp(email, otp);
+            authResponse.setMessage("OTP sent successfully");
+            authResponse.setStatus(200);
+            return new ResponseEntity<>(authResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            authResponse.setMessage("Failed to send OTP: " + e.getMessage());
+            authResponse.setStatus(500);
+            return new ResponseEntity<>(authResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    private Authentication authenticate(String username , String password) {
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody LoginRequest loginRequest) {
+        AuthResponse authResponse = new AuthResponse();
+        try {
+            String username = loginRequest.getEmail();
+            String password = loginRequest.getPassword();
+            String otp = loginRequest.getOtp();
+            UserDetails userDetails = customUserDetails.loadUserByUsername(username);
+            if (userDetails == null) {
+                authResponse.setMessage("User not found");
+                authResponse.setStatus(404);
+                return new ResponseEntity<>(authResponse, HttpStatus.NOT_FOUND);
+            }
+            Authentication authentication;
+            // Check if OTP is provided
+            if (otp != null && !otp.isEmpty()) {
+                if (!otpService.validateOtp(username, otp)) {
+                    // If OTP is not valid
+                    authResponse.setMessage("Invalid OTP");
+                    authResponse.setStatus(400);
+                    return new ResponseEntity<>(authResponse, HttpStatus.BAD_REQUEST);
+                }
+                authentication = new UsernamePasswordAuthenticationToken(username, null, userDetails.getAuthorities());
+            } else {
+//            authentication = authenticate(username, password);
+                if (passwordEncoder.matches(password, userDetails.getPassword())) {
+                    authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                } else {
+                    throw new BadCredentialsException("Invalid password");
+                }
+            }
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        UserDetails userDetails  = customUserDetails.loadUserByUsername(username);
+            String jwt = JwtProvider.generateToken(authentication);
+            authResponse.setJwt(jwt);
+            authResponse.setMessage("User Logged in successfully");
+            authResponse.setStatus(200);
 
-        if (userDetails == null){
-            throw new BadCredentialsException("invalid username");
+            return new ResponseEntity<>(authResponse, HttpStatus.OK);
+        } catch (BadCredentialsException e) {
+            authResponse.setMessage("Invalid credentials: " + e.getMessage());
+            authResponse.setStatus(401);
+            return new ResponseEntity<>(authResponse, HttpStatus.UNAUTHORIZED);
+        } catch (Exception e) {
+            authResponse.setMessage("Login failed: " + e.getMessage());
+            authResponse.setStatus(500);
+            return new ResponseEntity<>(authResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+
         }
-        if (!passwordEncoder.matches(password,userDetails.getPassword())){
-            throw new BadCredentialsException("invalid password");
-        }
-
-        return new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
     }
+
+//    private Authentication authenticate(String username , String password) {
+//
+//        UserDetails userDetails  = customUserDetails.loadUserByUsername(username);
+//
+//        if (userDetails == null){
+//            throw new BadCredentialsException("invalid username");
+//        }
+//        if (!passwordEncoder.matches(password,userDetails.getPassword())){
+//            throw new BadCredentialsException("invalid password");
+//        }
+//
+//        return new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+//    }
 }
