83b8670a007630d9a668f66de1063f1710299684
YASHPATEL\patel <patelyash2462@gmail.com>
2025-06-11 22:32:29 +0530

    Adding profile based configs and local and test profiles

--- src/main/java/com/projectmanagementsystembackend/config/SecurityConfig.java
+++ src/main/java/com/projectmanagementsystembackend/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.projectmanagementsystembackend.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,11 +26,20 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        boolean isTestProfile = Arrays.asList(env.getActiveProfiles()).contains("test");
 
+        if (isTestProfile){
+            http.csrf(AbstractHttpConfigurer::disable);
+            http.headers(AbstractHttpConfigurer::disable);
+        }
         http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request -> request.requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().permitAll())
                 .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
