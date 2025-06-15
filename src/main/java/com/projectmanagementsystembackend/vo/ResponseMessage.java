fe1fce562a20bffd8f50135c060c1e2ad426a30a
YASHPATEL\patel <patelyash2462@gmail.com>
2025-06-15 11:41:53 +0530

    Generic Interface to define Constant message to send

--- src/main/java/com/projectmanagementsystembackend/vo/ResponseMessage.java
+++ src/main/java/com/projectmanagementsystembackend/vo/ResponseMessage.java
@@ -20,4 +20,18 @@ public class ResponseMessage {
         this.message = message;
         this.status = status;
     }
+
+    public static ResponseMessage getServerError(Exception e){
+        return new ResponseMessage("Internal Server Error: " + e.getMessage(), 500);
+    }
+
+    public interface Generic {
+        ResponseMessage OTP_SENT = new ResponseMessage("OTP sent successfully", 200);
+        ResponseMessage ERROR_IN_SENDING_OTP = new ResponseMessage("Error in sending OTP", 500);
+        ResponseMessage DATA_SAVE_SUCCESS = new ResponseMessage("Data saved successfully", 200);
+        ResponseMessage DATA_UPDATE_SUCCESS = new ResponseMessage("Data updated successfully", 200);
+        ResponseMessage DATA_DELETE_SUCCESS = new ResponseMessage("Data deleted successfully", 200);
+        ResponseMessage DATA_NOT_FOUND = new ResponseMessage("Data not found", 404);
+        ResponseMessage INTERNAL_ERROR = new ResponseMessage("Something went wrong", 500);
+    }
 }
