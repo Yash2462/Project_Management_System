669f6f292b06f72df4faf4ba81ec037b8b77d173
YASHPATEL\patel <patelyash2462@gmail.com>
2025-06-15 11:11:36 +0530

    get host and port for redisconfig from properties file

--- src/main/java/com/projectmanagementsystembackend/config/RedisConfiguration.java
+++ src/main/java/com/projectmanagementsystembackend/config/RedisConfiguration.java
@@ -3,16 +3,23 @@ package com.projectmanagementsystembackend.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfiguration {
 
+    @Value("${redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${redis.port:6379}")
+    private String redisPort;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379");
+        config.useSingleServer().setAddress("redis://"+redisHost+":"+redisPort);
         return Redisson.create(config);
     }
 }
