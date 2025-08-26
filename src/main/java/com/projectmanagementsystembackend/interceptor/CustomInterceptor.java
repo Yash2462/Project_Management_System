09a4fa3f663143d5c95494ce28dbdf1764a6bd0e
yash.p <yash.p@techinnovative.net>
2025-08-26 16:36:52 +0530

    we can write custom logic to block request in interceptor like we can check subscription is active or expired

--- src/main/java/com/projectmanagementsystembackend/interceptor/CustomInterceptor.java
+++ src/main/java/com/projectmanagementsystembackend/interceptor/CustomInterceptor.java
@@ -1,16 +1,25 @@
 package com.projectmanagementsystembackend.interceptor;
 
+import com.projectmanagementsystembackend.model.User;
+import com.projectmanagementsystembackend.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
+@AllArgsConstructor
 public class CustomInterceptor implements HandlerInterceptor {
     // Implement methods from HandlerInterceptor as needed
     // For example, preHandle, postHandle, afterCompletion, etc.
+    private UserRepository userRepository;
 
     @Override
     public boolean preHandle(@NotNull HttpServletRequest request,
@@ -18,6 +27,12 @@ public class CustomInterceptor implements HandlerInterceptor {
                              @NotNull Object handler) throws Exception {
         //here we can write custom logic like if token is compromised then using path userId we can validate the request
         log.info("Incoming request: {} {}", request.getMethod(), request.getRequestURI());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            log.info("Authenticated user: {}", authentication.getName());
+        } else {
+            log.warn("Unauthenticated request");
+        }
         return true; // Continue with the request
     }
 
--- src/main/java/com/projectmanagementsystembackend/interceptor/WebMvcConfig.java
+++ src/main/java/com/projectmanagementsystembackend/interceptor/WebMvcConfig.java
@@ -1,15 +1,19 @@
 package com.projectmanagementsystembackend.interceptor;
 
+import com.projectmanagementsystembackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new CustomInterceptor())
+        registry.addInterceptor(new CustomInterceptor(userRepository))
                 .addPathPatterns("/**") // Specify the paths to intercept
                 .excludePathPatterns("/api/public/**"); // Exclude specific paths if needed
     }
